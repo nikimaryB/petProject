6456180bae2a1b4f4119009659397e6f4c4dc382
Nikita Boriskin <8858794@gmail.com>
2023-07-18 17:10:14 +0300

    add babel and i18n plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,22 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
+  const babelLoader = {
+    test: /\.(js|tsx|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract", { locales: ["ru", "en"], keyAsDefaultValue: true}
+          ]
+        ]
+      }
+    }
+  };
+  
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -48,6 +64,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     svgLoader,
     fileLoader,
+    // babelLoader, /// not need
     typescriptLoader,
     cssLoader,
   ]
