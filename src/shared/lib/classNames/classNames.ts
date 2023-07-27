12b025b7d92941b07c5a901bac6c2c72fb4cd0d2
Nikita Boriskin <8858794@gmail.com>
2023-07-27 22:35:51 +0300

    add jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -5,10 +5,11 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
+        ...Object.entries(mods)
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             .filter(([classNames, value]) => Boolean(value))
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             .map(([classNames,value]) => classNames)
-    ].join(' ');
+    ]
+        .join(' ');
 }
