1802fdc6d62cb2eaef47654faff07cead8c2b872
Nikita Boriskin <8858794@gmail.com>
2023-10-02 17:45:38 +0300

    aaa

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,30 +1,30 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-// import { buildCssLoader } from './loaders/buildCssLoader';
+// import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
-function buildCssLoader (isDev: boolean) {
-    return {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev? 'style-loader': MiniCssExtractPlugin.loader,
-            // "style-loader", //вместо него мини css? чтобы стили были отдельными файликами
-            // Translates CSS into CommonJS
-            {
-                loader:'css-loader',
-                options:{
-                    modules:{
-                        auto: (resPath: string) => Boolean(resPath.includes('module.')),
-                        localIdentName: isDev? '[path][name]__[local]': '[hash:base64:8]'
-                    },
-                }
-            },  
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
-}
+// function buildCssLoader (isDev: boolean) {
+//     return {
+//         test: /\.s[ac]ss$/i,
+//         use: [
+//             // Creates `style` nodes from JS strings
+//             isDev? 'style-loader': MiniCssExtractPlugin.loader,
+//             // "style-loader", //вместо него мини css? чтобы стили были отдельными файликами
+//             // Translates CSS into CommonJS
+//             {
+//                 loader:'css-loader',
+//                 options:{
+//                     modules:{
+//                         auto: (resPath: string) => Boolean(resPath.includes('module.')),
+//                         localIdentName: isDev? '[path][name]__[local]': '[hash:base64:8]'
+//                     },
+//                 }
+//             },  
+//             // Compiles Sass to CSS
+//             'sass-loader',
+//         ],
+//     };
+// }
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
